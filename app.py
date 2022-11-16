bc1c70fd70d9e739fbf8a181604d31b5318f22e4
PlugPyprch <plugpanyaprachum@gmail.com>
2022-11-16 17:51:39 +0700

    no cv

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import numpy as np
 from keras.models import load_model
-from tensorflow.keras.preprocessing import image
-import cv2
+from PIL import Image
 
 st.markdown("""
 <style>
@@ -21,11 +20,6 @@ st.title('Stage of Alzheimer You Are ? 🧠')
 
 model = load_model("./model_al.h5")
 
-def load_and_prep_image(filename, image_shape=224, scale=True):
-  img = image.load_img(filename, target_size=(image_shape, image_shape))
-  img = np.asarray(img)
-  return img/255.
-
 uploaded_file = st.file_uploader("Choose a image file", type=['jpg', 'png', 'ipeg'])
 
 class_names = ['MildDemented',
@@ -33,19 +27,18 @@ class_names = ['MildDemented',
                'NonDemented',
                'VeryMildDemented']
 
+def load_image(image_file):
+    img = Image.open(image_file)
+    resized = img.resize((224,224))
+    return resized
+
 
 if uploaded_file is not None:
-    # Convert the file to an opencv image.
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, 1)
-    opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-    # Now do something with the image! For example, let's display it:
-    st.image(opencv_image, channels="RGB")
-
-    resized = cv2.resize(opencv_image,(224,224))
+    resized = load_image(uploaded_file)
+    st.image(resized, channels="RGB")
 
     Genrate_pred = st.button("Generate Prediction")    
     if Genrate_pred:
         prediction = model.predict(np.expand_dims(resized, axis=0))
-        st.write('Predicted Label for the image is : v')
+        st.write('Predicted Label for the image is : ')
         st.title("{}".format(class_names[prediction.argmax()]))
